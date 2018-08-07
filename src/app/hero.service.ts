85b21f6f2532c0e16092caea0fed9672dd14ae61
Erik <erik.steinman@hotmail.com>
2018-08-07 10:36:39 +0200

    add in-memory-data-service, add injects and functions

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,15 +5,17 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from  '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  
+  private heroesURL = 'api/heroes';
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of (HEROES);
+    return this.http.get<Hero[]>(this.heroesURL)
   }
 
   getHero(id: number): Observable<Hero> {
@@ -22,5 +24,11 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   };
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient, 
+    private messageService: MessageService) { }
+  
+    private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 }
